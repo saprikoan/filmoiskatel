4e030c697d0e5be0af5f64f64b92cfbecbcdf187
Oleg-Dainovich <olegdainovich@gmail.com>
2024-10-16 02:35:01 +0300

    feat: init aside header component

---
+++ src/ui/components/AsideHeader/AsideHeader.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import { AsideHeader as Header } from '@gravity-ui/navigation';
+
+import { ICON } from './constants/constants.tsx';
+import { MENU_ITEMS } from './constants/constants.tsx';
+
+export const AsideHeader = () => {
+    const initialCompact = localStorage.getItem('asideHeaderCompact') == 'true';
+    const [compact, setCompact] = useState(initialCompact);
+
+    useEffect(() => {
+        localStorage.setItem('asideHeaderCompact', String(compact))
+    }, [compact])
+
+    return(
+        <Header 
+            compact={compact} onChangeCompact={setCompact} 
+            headerDecoration={true}
+            logo={ICON}
+            menuItems={MENU_ITEMS}
+        />
+    );
+}
